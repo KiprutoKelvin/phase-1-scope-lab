10368c0238d90692e75eb903a9c5716170067aa7
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-19 02:12:42 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob" ;
+function findCustomerName() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
+const leastFavoriteCustomer = 'Epic';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Dull';
+  }
+  changeLeastFavoriteCustomer();
